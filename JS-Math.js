328c0f7aea88c71a73e0ad311c7c532e41a30a42
Pranav Trivedi <pranavaryans@gmail.com>
2021-12-22 08:32:11 +0530

    Create JS-Math.js

---
+++ JS-Math.js
@@ -0,0 +1,14 @@
+<script>
+	var a = 10.5;
+	var b = 10;
+	var c = 20;
+	document.write("ceil() : "+Math.ceil(a)+"<br>");
+	document.write("floor() : "+Math.floor(a)+"<br>");
+	document.write("abs() : "+Math.abs(a)+"<br>");
+	document.write("max() : "+Math.max(a,b,c)+"<br>");
+	document.write("min() : "+Math.min(a,b,c)+"<br>");
+	document.write("random() : "+Math.random()+"<br>");
+	document.write("round() : "+Math.round(a)+"<br>");
+	document.write("pow() : "+Math.pow(2,4)+"<br>");
+	document.write("sqrt() : "+Math.sqrt(25)+"<br>");
+</script>
