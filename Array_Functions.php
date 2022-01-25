4282436e8e7a170e9acab0e62424db185c2c082b
Pranav Trivedi <pranavaryans@gmail.com>
2022-01-25 10:43:37 +0530

    Update Array_Functions.php

--- Array_Functions.php
+++ Array_Functions.php
@@ -25,7 +25,7 @@
 	$a2=array("blue","yellow");
 	print_r(array_merge($a2,$a1));
 	echo "</br>";
-	$a=array("a"=>"Volvo","b"=>"BMW","c"=>"Toyota");
+	$a=array("a"=>"Volvo","b"=>"BMW","c"=>"Toyota","d"=>"Toyota");
 	print_r(array_reverse($a));
 	echo "</br>";
 	
@@ -37,4 +37,31 @@
 	
 	echo array_shift($a)."</br>";
 	print_r(array_slice($a,0));
+	echo "</br>";
+	print_r(array_unique($a));
+	array_unshift($a,"Volvo");
+	echo "<br>";
+	print_r($a);
+	echo "<br>";
+	print_r(array_keys($a));
+	echo "<br>";
+	if (array_key_exists("d",$a))
+	{
+		echo "Key exists!";
+	}
+	else
+	{
+		echo "Key does not exist!";
+	}
+	echo "<br>";
+	array_push($a,"BLUE","YELLOW");
+	print_r($a);
+	echo "<br>";
+	array_pop($a);
+	print_r($a);
+	echo "<br>";
+	array_multisort($a);
+	print_r($a);	
+	echo "<br>";
+	echo array_search("BLUE",$a);
 ?>
